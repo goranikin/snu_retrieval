a62460cff3055d2d1411575cdcfa1e2ed620990c
goranikin <goranikin@snu.ac.kr>
2025-05-31 22:40:55 +0900

    🐛 Fix a bug

--- src/utils/save_model_to_vesslai.py
+++ src/utils/save_model_to_vesslai.py
@@ -1,11 +1,12 @@
+import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model_name = "Qwen/Qwen3-14B"
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(
-    model_name, torch_dtype="auto", device_map="auto"
-)
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto").to(device)
 
 model.save_pretrained("/output/my_model")
 tokenizer.save_pretrained("/output/my_model")
